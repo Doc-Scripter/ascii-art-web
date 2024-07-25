351eb9aeef56868a6892a53b06fe51f98ae8e956
cliffootieno <clifford242526@gmail.com>
2024-07-25 18:13:06 +0300

    refactor : Add port number display

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
@@ -8,20 +9,19 @@ import (
 )
 
 func main() {
-	if len(os.Args)!= 1 {
+	if len(os.Args) != 1 {
 		return
 	}
 
 	fs := http.FileServer(http.Dir("static"))
 	assets := http.FileServer(http.Dir("assets"))
 
-	
 	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
 	http.HandleFunc("/ascii-art", handler.ArtHandler)
 	http.HandleFunc("/about", handler.AboutHandler)
 	http.HandleFunc("/instructions", handler.InstructionsHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.URL.Path {
 		case "/", "/home":
@@ -30,6 +30,6 @@ func main() {
 			handler.ErrorPageHandler(w, http.StatusNotFound, "404 - Not Found")
 		}
 	})
-
+	fmt.Println("Server is running on port :8080")
 	http.ListenAndServe(":8080", nil)
 }
